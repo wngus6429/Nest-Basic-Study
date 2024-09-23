f67e7c61cfd662e289b2444574edaa58d4a5989e
ParkJuhyun <wngus6429@gmail.com>
2024-09-24 00:50:28 +0900

    Update app.e2e-spec.ts

--- 니콜라스 기초/test/app.e2e-spec.ts
+++ 니콜라스 기초/test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -12,13 +12,82 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // 데코레이터가 없는 속성은 제거
+        forbidNonWhitelisted: true, // 데코레이터가 없는 속성이 있으면 요청 자체를 막음
+        transform: true, // 요청에서 넘어온 자료들의 자동 형변환 미쳤따리
+      }),
+    );
     await app.init();
   });
+  // Nestjs는 테스트 마다 어플리케이션을 생성함.
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  describe('/movies (GET)', () => {
+    it('/movies (GET)', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+  });
+
+  describe('/movies/:id', () => {
+    beforeEach(async () => {
+      // 필요한 데이터를 사전에 생성
+      await request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test Movie',
+          year: 2000,
+          genres: ['action'],
+        });
+    });
+    // it.todo('GET');
+    // it.todo('DELETE');
+    // it.todo('PATCH');
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
