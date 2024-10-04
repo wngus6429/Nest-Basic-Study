4f8cd2d87999b3f8a48ae03b008fe3427001ac5d
Park Juhyun <wngus6429@gmail.com>
2024-10-05 02:33:48 +0900

    해당 유저 조건

--- 존안/start-nest/src/boards/boards.controller.ts
+++ 존안/start-nest/src/boards/boards.controller.ts
@@ -16,6 +16,8 @@ import { CreateBoardDto } from './dto/create-board.dto';
 import { Board } from './board.entity';
 import { BoardStatus } from './board-status.enum';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 
 @Controller('boards')
 @UseGuards(AuthGuard())
@@ -27,6 +29,11 @@ export class BoardsController {
     return this.boardsService.getAllBoards();
   }
 
+  @Get('/userBoards')
+  getUserBoards(@GetUser() user: User): Promise<Board[]> {
+    return this.boardsService.getUserBoards(user);
+  }
+
   @Get('/:id')
   getBoardById(@Param('id') id: number): Promise<Board> {
     return this.boardsService.getBoardById(id);
@@ -34,13 +41,24 @@ export class BoardsController {
 
   @Post()
   @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
-    return this.boardsService.createBoard(createBoardDto);
+  createBoard(
+    @Body() createBoardDto: CreateBoardDto,
+    @GetUser() user: User,
+  ): Promise<Board> {
+    return this.boardsService.createBoard(createBoardDto, user);
   }
 
+  // @Delete('/:id')
+  // deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
+  //   return this.boardsService.deleteBoard(id);
+  // }
+
   @Delete('/:id')
-  deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
-    return this.boardsService.deleteBoard(id);
+  deleteBoard(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.boardsService.deleteBoard(id, user);
   }
 
   @Patch('/:id/status')
--- 존안/start-nest/src/boards/boards.service.ts
+++ 존안/start-nest/src/boards/boards.service.ts
@@ -5,9 +5,13 @@ import { CreateBoardDto } from './dto/create-board.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Board } from './board.entity';
 import { Repository } from 'typeorm';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class BoardsService {
+  createQueryBuilder(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
   constructor(
     @InjectRepository(Board) private boardRepository: Repository<Board>,
   ) {}
@@ -15,13 +19,27 @@ export class BoardsService {
   async getAllBoards(): Promise<Board[]> {
     return await this.boardRepository.find();
   }
+  // async getUserBoards(user: User): Promise<Board[]> {
+  //   return await this.boardRepository.find({ where: { user } });
+  // }
 
-  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+  async getUserBoards(user: User): Promise<Board[]> {
+    const query = this.boardRepository.createQueryBuilder('board');
+    query.where('board.userId = :userId', { userId: user.id });
+    const boards = await query.getMany();
+    return boards;
+  }
+
+  async createBoard(
+    createBoardDto: CreateBoardDto,
+    user: User,
+  ): Promise<Board> {
     const { title, description } = createBoardDto;
     const board = this.boardRepository.create({
       title,
       description,
       status: BoardStatus.PUBLIC,
+      user,
     });
     await this.boardRepository.save(board);
     return board;
@@ -38,10 +56,10 @@ export class BoardsService {
     return found;
   }
 
-  async deleteBoard(id: number): Promise<void> {
+  async deleteBoard(id: number, user: User): Promise<void> {
     // remove쓰면 데이터가 있는지 확인해야해서 코드가 길어짐
     // delete는 있으면 지우고 없으면 아무것도 안함
-    const result = await this.boardRepository.delete({ id });
+    const result = await this.boardRepository.delete({ id, user }); // 해당 유저만 삭제하게끔
     // 지웠으면 affected가 1이상, 없으면 0
     if (result.affected === 0) {
       throw new NotFoundException(`Can't find Board with id ${id}`);
